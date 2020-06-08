9be813e5c7a6b110d4a18ad1a783fbbc8b0f4ef4
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-08 17:30:12 +0100

    tailwindcss setup, buttons, forms, tabs component created

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
-module.exports = {
+let environment = {
   plugins: [
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -10,3 +11,21 @@ module.exports = {
     })
   ]
 }
+
+// To reduce the size when in production
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+
+module.exports = environment;
