89efad16c15a57510a28eab633275c36169a465e
Abubakar Diallo <dialloabubakar5861@gmail.com>
2020-06-10 21:41:37 +0100

    added validation to the models

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,9 @@
 class Ticket < ApplicationRecord
+  validates :title, presence: true
+  validates :message, presence: true
+  validates :category, presence: true
+  validates :priority, presence: true
+
   belongs_to :user
   has_many :comments, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,4 +10,10 @@ class User < ApplicationRecord
 
   has_many :tickets, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  validates :full_name, presence: true
+  validates :username, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
